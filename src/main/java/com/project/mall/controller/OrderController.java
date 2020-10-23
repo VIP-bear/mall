e7eee6d46a88ebc8e46d9322224ed5f9434bfe3a
1710/sy <1802272314@qq.com>
2020-10-23 13:24:29 +0800

    完善订单功能的controller层

--- src/main/java/com/project/mall/controller/OrderController.java
+++ src/main/java/com/project/mall/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.project.mall.controller;
 
 import com.project.mall.controller.req.buyer.CheckOrderReq;
 import com.project.mall.controller.req.buyer.PurchsaeReq;
+import com.project.mall.controller.req.buyer.QueryOrderReq;
 import com.project.mall.controller.res.ReqResult;
 import com.project.mall.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
@@ -24,12 +25,20 @@ public class OrderController {
     @ResponseBody
     public ReqResult placeOrder(PurchsaeReq purchsaeReq) {
         log.info("purchaseMessage: {}" + purchsaeReq);
-        return orderService.placeOrder(purchsaeReq);
+        return orderService.addOrder(purchsaeReq);
     }
+
+    /**
+     * 修改订单状态
+     * @param ID
+     * @param order_state
+     * @return
+     */
     @PutMapping("/changeOrderState")
     @ResponseBody
-    public ReqResult changeOrderState(@RequestParam(name = "order_id")Long ID) {
-        return null;
+    public ReqResult changeOrderState(@RequestParam(name = "order_id")Long ID,
+                                      @RequestParam(name = "order_state")String order_state) {
+        return orderService.updateOrderState(ID,order_state);
     }
 
     /**
@@ -41,6 +50,23 @@ public class OrderController {
     @ResponseBody
     public ReqResult checkOrderByBuyerID(@RequestParam(name = "buyer_name")Long ID) {
 
-        return null;
+        return orderService.getOrder(ID);
+    }
+
+    /**
+     * 按商品状态查询商品
+     * @param queryOrderReq
+     * @return
+     */
+    @GetMapping("/checkOrder/checkOrderByOrderState")
+    @ResponseBody
+    public ReqResult checkOrderByOrderState(QueryOrderReq queryOrderReq) {
+        return orderService.getOrderByState(queryOrderReq);
+    }
+
+    @DeleteMapping("/deleteOrder")
+    @ResponseBody
+    public ReqResult deleteOrder(@RequestParam(name = "order_id")Long ID) {
+        return orderService.deleteOrder(ID);
     }
 }
